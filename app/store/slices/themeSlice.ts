1aeb89615d64d33d87f340b7fbcb0847d0cf6c90
ethanjags <ethanjags@berkeley.edu>
2024-07-17 01:47:04 -0500

    add themeslice

---
+++ app/store/slices/themeSlice.ts
@@ -0,0 +1,25 @@
+// themeSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const getInitialTheme = () => {
+  const storedTheme = localStorage.getItem('theme');
+  if (storedTheme) {
+    return storedTheme;
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState: getInitialTheme(),
+  reducers: {
+    toggleTheme: (state) => {
+    //   const newTheme = state === 'light' ? 'dark' : 'light';
+    const newTheme = 'light';
+      return newTheme;
+    },
+  },
+});
+
+export const { toggleTheme } = themeSlice.actions;
+export default themeSlice.reducer;
