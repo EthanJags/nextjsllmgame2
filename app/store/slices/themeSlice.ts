672600cb518b2f4b2e022f862f14c13ac1742933
ethanjags <ethanjags@berkeley.edu>
2024-07-17 01:53:50 -0500

    set theme to light

--- app/store/slices/themeSlice.ts
+++ app/store/slices/themeSlice.ts
@@ -1,17 +1,17 @@
 // themeSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
-const getInitialTheme = () => {
-  const storedTheme = localStorage.getItem('theme');
-  if (storedTheme) {
-    return storedTheme;
-  }
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-};
+// const getInitialTheme = () => {
+//   const storedTheme = localStorage.getItem('theme');
+//   if (storedTheme) {
+//     return storedTheme;
+//   }
+//   return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+// };
 
 const themeSlice = createSlice({
   name: 'theme',
-  initialState: getInitialTheme(),
+  initialState: 'light',
   reducers: {
     toggleTheme: (state) => {
     //   const newTheme = state === 'light' ? 'dark' : 'light';
